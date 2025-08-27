523baef66ee1ab04ea4f40057239a17ed44f6a72
pharuxtan <28842091+pharuxtan@users.noreply.github.com>
2025-08-27 20:22:15 +0200

    fix(machines_tooltip): fix energy amounts

--- src/main/java/wootrevived/woot/blocks/fluid_infuser/FluidInfuserBlock.java
+++ src/main/java/wootrevived/woot/blocks/fluid_infuser/FluidInfuserBlock.java
@@ -96,7 +96,7 @@ public class FluidInfuserBlock extends Block implements EntityBlock {
                         .append(Component.translatable("info.woot_revived.power").append(Component.literal(": ")).setStyle(MACHINE_STYLE))
                         .append(Component.literal(WootContainerScreen.formatInteger(component.energy())))
                         .append(Component.literal("/").setStyle(MACHINE_STYLE))
-                        .append(WootContainerScreen.formatInteger(Config.DyeLiquifier.ENERGY_CAPACITY))
+                        .append(WootContainerScreen.formatInteger(Config.FluidInfuser.ENERGY_CAPACITY))
                         .append(Component.literal(" FE").setStyle(UNIT_STYLE))
         );
 
