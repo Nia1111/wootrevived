ac5c2d33d36c1fe0380a2ae225a1f8fe19445d7e
pharuxtan <28842091+pharuxtan@users.noreply.github.com>
2025-10-13 17:52:59 +0200

    fix(dye_liquifier): always try to do pure dye fluid

--- src/main/java/wootrevived/woot/blocks/dye_liquifier/DyeLiquifierBlockEntity.java
+++ src/main/java/wootrevived/woot/blocks/dye_liquifier/DyeLiquifierBlockEntity.java
@@ -82,6 +82,7 @@ public class DyeLiquifierBlockEntity extends WootMachineBlockEntity implements M
         if(level.isClientSide)
             return;
 
+        generatePureFluid();
         tickFluid(outputTankHandler, pos, side -> getProperties(side).getOutputFluidProperty());
     }
 
@@ -285,7 +286,6 @@ public class DyeLiquifierBlockEntity extends WootMachineBlockEntity implements M
             inventoryHandler.extractItem(INPUT_SLOT, ingredientAmount, false);
         }
 
-        generatePureFluid();
         setChanged();
     }
 
